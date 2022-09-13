78c2abe58a1924609a79b646512bd42ca689049b
VIBOL.KHAN <VIBOL.KHAN@pnc.lan>
2022-09-13 23:08:12 +0700

    merge branch

--- front/src/views/Alumni/AlumniProfileView.vue
+++ front/src/views/Alumni/AlumniProfileView.vue
@@ -76,7 +76,7 @@
       },
       //=================== edit alumni experience ===================
       editExperience(data) {
-        axios.put(this.url+'alumniWork/2'+data.id, data)
+        axios.put(this.url+'alumniWork/'+data.id, data)
         .then((response) => {
           console.log(response.data);
           this.getData();
@@ -92,7 +92,7 @@
       },
       //=================== get  alumni experience ===================
       getData() {
-        axios.get(this.url+"alumni/2").then((res) => {
+        axios.get(this.url+"alumni/1").then((res) => {
           this.alumniData = res.data;
           this.alumniInfo=res.data.user
           this.alumniExperiences=res.data.work_experience
