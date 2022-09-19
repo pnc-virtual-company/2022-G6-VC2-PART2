f6b7616a6e2ca3543c4987ee5e0f93ce548afd0b
hengheak <heng.heak@student.passerellesnumeriques.org>
2022-09-19 09:30:34 +0700

    #46 register alumni

--- front/src/components/Register/FormRegister.vue
+++ front/src/components/Register/FormRegister.vue
@@ -1,76 +1,74 @@
 <template>
-    <div class="mt-10 block p-5 rounded-lg shadow-lg bg-white max-w-md m-auto">
-      <form>
-        <div
-          class="w-full mb-10 flex justify-center py-3 bg-blue-500 text-white font-medium rounded"
-        >
-          REGISTER
-        </div>
-        <div class="grid grid-cols-2 gap-4">
-          <div class="mb-6">
-            <input
-              type="text"
-              class="w-full px-3 py-1.5 border border-gray-300 rounded focus:border-blue-600 focus:outline-none"
-              placeholder="First name"
-            />
-          </div>
-          <div class="mb-6">
-            <input
-              type="text"
-              class="w-full px-3 py-1.5 border border-gray-300 rounded focus:border-blue-600 focus:outline-none"
-              placeholder="Last name"
-            />
-          </div>
-        </div>
-        <div class="mb-6">
-          <input
-            type="email"
-            class="w-full px-3 py-1.5 border rounded focus:border-blue-600 focus:outline-none"
-            placeholder="Email address"
+  <section class="h-screen">
+      <div class="flex justify-center items-center p-[3%]">
+        <div class="md:w-8/12 lg:w-6/12 md:mb-0">
+          <img
+            src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
+            class="w-full"
+            alt="Phone image"
           />
         </div>
-        <div class="mb-6">
-          <input
-            type="password"
-            class="w-full px-3 py-1.5 border rounded focus:border-blue-600 focus:outline-none"
-            placeholder="Password"
-          />
-        </div>
-          <div class="mb-3 flex justify-between">
-            <select
-              class="px-2 py-1 text-sm border border-gray-300 rounded focus:text-gray-700 focus:border-blue-600"
+        <div class="md:w-8/12 lg:w-5/12 lg:ml-20 p-[5%] px-5 mr-[10%] shadow-xl">
+          <form>
+            <div
+              class="w-full mb-10 flex justify-center py-3 bg-blue-500 text-white font-medium"
             >
-              <option selected>Gender</option>
-              <option value="male">Male</option>
-              <option value="male">Female</option>
-            </select>
+              REGISTER
+            </div>
+            <div class="grid grid-cols-2 gap-4 w-[90%] m-auto">
+              <div class="mb-6">
+                <input
+                  type="text"
+                  class="w-full px-3 py-1.5 border border-gray-300 rounded focus:border-blue-600 focus:outline-none"
+                  placeholder="First name"
+                />
+              </div>
+              <div class="mb-6">
+                <input
+                  type="text"
+                  class="w-full px-3 py-1.5 border border-gray-300 rounded focus:border-blue-600 focus:outline-none"
+                  placeholder="Last name"
+                />
+              </div>
+            </div>
+            <div class="mb-6 w-[90%] m-auto">
               <input
-                type="radio"
-                name="inlineRadioOptions"
-                value="option1"
+                type="email"
+                class="w-full px-3 py-1.5 border rounded focus:border-blue-600 focus:outline-none"
+                placeholder="Email address"
               />
-              <label>ERO</label
-              >
+            </div>
+            <div class="mb-6 m-auto w-[90%]">
               <input
-                type="radio"
-                name="inlineRadioOptions"
-                value="option2"
+                type="password"
+                class="w-full px-3 py-1.5 border rounded focus:border-blue-600 focus:outline-none"
+                placeholder="Password"
               />
-              <label>ALUMNI</label
+            </div>
+            <div class="mb-3 flex justify-between m-auto w-[90%]">
+              <select
+                class="px-4 border rounded"
               >
+                <option selected>Gender</option>
+                <option value="male">Male</option>
+                <option value="male">Female</option>
+              </select>
+              <button
+              type="submit"
+              class="flex justify-center m-auto bg-blue-500 text-white w-[40%] py-1 border rounded ml-[30%]"
+            >
+              CREATE
+            </button>
+            </div>
+            
+          </form>
         </div>
-        <button
-          type="submit"
-          class="flex justify-center m-auto bg-blue-500 text-white w-[40%] py-2 border rounded"
-        >
-          CREATE
-        </button>
-      </form>
-    </div>
-  </template>
-  
-  <script>
-  export default {};
-  </script>
-  
-  <style></style>
+      </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home">
+  <div class="home flex justify-center">
     <img alt="Vue logo" src="../assets/logo.png" width="100">
     <HelloWorld msg="WELCOME TO VIRTUAL COMPANY II 2022"/>
   </div>
