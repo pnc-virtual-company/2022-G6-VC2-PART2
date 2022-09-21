cc0b1a7c5d98013849594407b227500e4208162e
OUN-BAV <oun.bav@student.passerellesnumeriques.org>
2022-09-21 09:37:27 +0700

    #51 delete alumni: delete and validate form invite

--- front/src/views/Ero/ListALumniCard.vue
+++ front/src/views/Ero/ListALumniCard.vue
@@ -3,7 +3,8 @@
         <h1 class="text-3xl text-gray-900 dark:text-white ml-7" >Alumni List</h1>
         <div class=" flex justify-between items-center w-[95%] mx-auto border-l-4 border-blue-600 rounded-lg p-2.5 shadow-lg bg-slate-200 mt-3" v-for="alumni of alumniData" :key="alumni">
             <div>
-                <img src="@/assets/girl.webp" alt="" class="w-20 h-20 rounded-full">
+                <img src="@/assets/girl.webp" alt="" class="w-20 h-20 rounded-full" v-if="alumni.gender=='Female'">
+                <img src="@/assets/boy.jpg" alt="" class="w-20 h-20 rounded-full" v-if="alumni.gender=='Male'">
             </div>
             <div>
                 <p>Name</p>
@@ -15,7 +16,7 @@
             </div>
             <div>
                 <p>Action</p>
-                <fa icon="trash-alt" class="text-[1.2rem] text-[#e04] bg-[#ddd]  p-2 rounded-full cursor-pointer" />
+                <fa icon="trash-alt" class="text-[1.2rem] text-[#e04] bg-[#ddd]  p-2 rounded-full cursor-pointer" @click="deletAlumni(alumni.user.id,alumni.id)"/>
             </div>
         </div>
     </div>
@@ -23,6 +24,11 @@
 <script>
 
 export default({
-    props:{ alumniData:Array }
+    props:{ alumniData:Array },
+    methods: {
+        deletAlumni(userId,alumniId){
+            this.$emit('toDeleteAlumni',userId, alumniId)
+        }
+    }
 })
 </script>
--- front/src/views/Ero/ListAlumniView.vue
+++ front/src/views/Ero/ListAlumniView.vue
@@ -4,11 +4,12 @@
          @invite = "show = true"
         />
         <div class="mt-4 mx-auto bg-slate-50 rounded-lg p-2.5 ">
-            <CardAlumni :alumniData='alumniData'/>
+            <CardAlumni :alumniData='alumniData' @toDeleteAlumni="removeAlumni"/>
         </div>
     </div> 
     <section v-if="show" class="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full flex items-center"> 
         <InvitAlumniForm
+            :isExisted="isexisted"
             @hideForm="hide"
             @CreateAndInviteAlumni = "CreateAndInviteAlumni"
         /> 
@@ -25,6 +26,7 @@ import axios from '../../axios-http'
 import filterAlumni from "../Ero/FilterAlumni.vue"
 import CardAlumni from "../Ero/ListALumniCard.vue"
 import InvitAlumniForm from './InvitAlumniForm.vue'
+import swal from 'sweetalert2';
 export default ({
     components: {filterAlumni,CardAlumni,InvitAlumniForm},
     data(){
@@ -32,12 +34,14 @@ export default ({
             alumniData:[],
             show: false,
             isSpin: false,
+            isexisted:false,
         }
     },
     methods:{
         getAlumni(){
             axios.get('alumni').then((res)=>{
                 this.alumniData=res.data
+                console.log(this.alumniData)
             }).catch((error)=>{
                 console.log(error);
             })
@@ -51,11 +55,32 @@ export default ({
             this.isSpin= true
             let user = {firstName:firstName,lastName:lastName,email:email,password:'12345678',role:'alumni'};
             axios.post('user', user).then(()=>{
-                this.isSpin= false;
                 this.show = false;
+                this.isSpin= false;
                 this.getAlumni();
+            }).catch((error)=>{
+                this.isSpin= false;
+                this.isexisted=true;
+                console.log(error.response.data.message)
             })
-        }
+        },
+        removeAlumni(userId){
+            swal.fire({
+                    title: 'Are you sure to delete?',
+                    text: "You won't be able to revert this!",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#0062ff',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Yes'
+                }).then((result) => {
+                    if (result.isConfirmed) {       
+                        axios.delete('user/'+userId).then(()=>{
+                            this.getAlumni();
+                        })
+                    }
+                })
+        }, 
     },
     created(){
         this.getAlumni()
